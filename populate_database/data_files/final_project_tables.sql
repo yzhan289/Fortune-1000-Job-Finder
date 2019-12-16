74d11c0a72b20098e130ef3c801368b08473f13c
Kiho Kwon <kkwon14@jhu.edu>
2019-12-15 22:12:30 -0500

    Update relational table definition.
    Updated the table definitions with specifying primary keys and not null attribtes.
    Also, updated the README to be clear which direction create_tables.py needs to be run from.

--- populate_database/data_files/final_project_tables.sql
+++ populate_database/data_files/final_project_tables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS Company;
 CREATE TABLE Company (
-name            VARCHAR(40),
 rank            INTEGER,
+name            VARCHAR(40) NOT NULL,
 change_in_rank  INTEGER,
 profit_mil      FLOAT,
 profit_change   FLOAT,
@@ -12,13 +12,14 @@ industry        VARCHAR(46),
 hq_state        VARCHAR(21),
 hq_city         VARCHAR(25),
 latitude        FLOAT,
-longitude       INTEGER
+longitude       INTEGER,
+PRIMARY KEY (name)
 );
 
 DROP TABLE IF EXISTS CityInfo;
 CREATE TABLE CityInfo (
-state_name              VARCHAR(21),
-city_name               VARCHAR(25),
+state_name              VARCHAR(21) NOT NULL,
+city_name               VARCHAR(50) NOT NULL,
 city_population         INTEGER,
 num_murders             INTEGER,
 num_rapes               INTEGER,
@@ -27,16 +28,18 @@ num_assaults            INTEGER,
 num_burglaries          INTEGER,
 num_larcenies           INTEGER,
 num_motor_thefts        INTEGER,
-num_arsons              INTEGER
+num_arsons              INTEGER,
+PRIMARY KEY (state_name, city_name)
 );
 
 DROP TABLE IF EXISTS StateInfo;
 CREATE TABLE StateInfo (
-state_name          VARCHAR(21),
-state_code          VARCHAR(2),
+state_name          VARCHAR(21) NOT NULL,
+state_code          VARCHAR(2) NOT NULL,
 dollar_parity       FLOAT,
 goods_parity        FLOAT,
-rent_parity         FLOAT
+rent_parity         FLOAT,
+PRIMARY KEY (state_name)
 );
 
 DROP VIEW IF EXISTS CompactCrimeData;
@@ -44,7 +47,7 @@ CREATE VIEW CompactCrimeData AS
 SELECT
 	state_name,
 	city_name,
-	(num_murders + num_rapes + num_robbery + num_assaults) AS num_violent_crime,
-	(num_burglaries + num_larcenies + num_motor_thefts + num_arsons) AS num_property_crime,
-	(num_murders + num_rapes + num_robbery + num_assaults + num_burglaries + num_larcenies + num_motor_thefts + num_arsons) AS total_crimes
+	((num_murders + num_rapes + num_robbery + num_assaults) / city_population * 100000) AS violent_crimes_per_100000,
+	((num_burglaries + num_larcenies + num_motor_thefts + num_arsons) / city_population * 100000) AS property_crime_per_100000,
+	((num_murders + num_rapes + num_robbery + num_assaults + num_burglaries + num_larcenies + num_motor_thefts + num_arsons) / city_population * 100000) AS crimes_per_100000
 FROM CityInfo;
