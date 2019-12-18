140795668b9a7348f6a89c9e2692fa622826e093
Andrew Zhang <Andrew@Andrews-MacBook-Pro.local>
2019-12-18 13:14:39 -0500

    Fixing query

--- quiz.php
+++ quiz.php
@@ -24,11 +24,10 @@
 
     <?php
     include 'open.php';
-    $query = "SELECT * FROM Company \n"; # Base query with some true condition
+    $query = "SELECT * FROM Company WHERE 1 = 1\n"; # Base query with some true condition
 
-    $state = $_POST["state"];
     if ($state) {
-      $state_condition = "AND Company.state = '%state'\n", $state;
+      $state_condition = "AND Company.state = '$state'\n";
       $query .= $state_condition;
     }
 
